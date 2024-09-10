550a791b5464061e95cc8ca3924b1b3045e0d666
Lucas Coutinho <l7coutinho.dev@gmail.com>
2024-09-10 17:32:36 -0300

    checkstyle fix

--- src/main/java/com/betrybe/agrix/security/JwtFilter.java
+++ src/main/java/com/betrybe/agrix/security/JwtFilter.java
@@ -6,15 +6,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-import java.util.Optional;
 
 /**
  * Class: JWT Filter.
